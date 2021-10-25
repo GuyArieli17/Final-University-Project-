5c45943ba03ae233231263e55c967dc29cd4529b
Amit-Nadam <73649320+Amit-Nadam@users.noreply.github.com>
2021-10-25 10:12:59 +0300

    fixed the files according to possible actions

--- src/test.py
+++ src/test.py
@@ -7,9 +7,14 @@ from json_handler import JsonHandler
 load_dotenv()
 
 eng = cityflow.Engine(os.getenv("CONFIG_JSON_FILE"))
+lst = 5 #number of lightphases in roadnet file
+j=0
 for i in range(5000):
-    if i == 200:
-        eng.set_tl_phase("intersection_1_1", 0)
+    if j >= lst:
+        j = 0
+    if i % 50:
+        eng.set_tl_phase("intersection_1_1", j)
+        j+=1
 
     eng.next_step()
 

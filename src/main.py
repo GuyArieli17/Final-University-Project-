e52478329a5914432146765209d7797815723c33
GuyArieli17 <guyarieli17@gmail.com>
2021-11-17 20:40:18 +0200

    make cityflow api

--- src/main.py
+++ src/main.py
@@ -7,4 +7,9 @@ import os
 load_dotenv()
 config = os.getenv("CONFIG_JSON_FILE")
 api = Cityflow_API(config)
-api.start()
+for i in range(100):
+    if i == 50:
+        api.set_action(('intersection_1_1',1))
+    if i == 99:
+        print(api.get_state())
+    api.next_frame()
--- src/public/API.py
+++ src/public/API.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cityflow
-
+import random
 
 class Cityflow_API:
 
@@ -8,12 +8,7 @@ class Cityflow_API:
         self.eng = cityflow.Engine(config) # city flow engine
         self.n_steps = n_steps
         self.dimension = np.array(dimension) # set the diminstion we will use
-
-    def start(self):
-        for i in range(100):
-            if i == 99:
-                print(self.get_state())
-            self.next_frame()
+        self.number_of_state_delta = 0
 
     def next_frame(self)->None:
         self.eng.next_step()
@@ -24,23 +19,30 @@ class Cityflow_API:
         vehicle_lst = self.eng.get_vehicles(include_waiting=False)
         dict_by_lane = self.eng.get_lane_vehicle_count()
         dict_by_waiting = self.eng.get_lane_waiting_vehicle_count()
-        state["n_vehicle"] = n_vehicle
-        state["vehicle"] = vehicle_lst
-        state["dict_by_lane"] = dict_by_lane
-        state["dict_by_waiting"] = dict_by_waiting
-        # get_lane_waiting_vehicle_count()
+        avg_travel_time = self.eng.get_average_travel_time()
+        # state["n_vehicle"] = n_vehicle
+        # state["vehicle"] = vehicle_lst
+        # state["dict_by_lane"] = dict_by_lane
+        # state["dict_by_waiting"] = dict_by_waiting
+        state["avg_travel_time"] = avg_travel_time
         # get_lane_vehicles()
         # get_vehicle_info(vehicle_id)
         # get_vehicle_speed()
         # get_vehicle_distance()
         # get_leader(vehicle_id)
         # get_current_time()
-#         # get_average_travel_time()
         return state
-#         
 
-#     def set_action(self,action:np.array)->None:
-#         pass
+    def stochastic(self):
+        #TODO: implemnt random routs from randoms cars
+        rnd_num = random.random()
+        vehicle_id = None
+        route = None
+        speed = 0
+        self.eng.set_vehicle_route(vehicle_id,route)
+        self.eng.set_vehicle_speed(vehicle_id, speed)
 
-#     def next_frame(self)->None:
-#         self.eng.next_step()
+    def set_action(self,action:list)->None:
+        #TODO: How to autamte network without changing the json
+        intersection_id , phase_id = action
+        self.eng.set_tl_phase(intersection_id, phase_id)

6006140f914da1339a7abde8e8d747fa918437ff
Amit-Nadam <73649320+Amit-Nadam@users.noreply.github.com>
2021-11-21 10:08:58 +0200

    Update all actions which exists

--- src/mediator/actions.py
+++ src/mediator/actions.py
@@ -1,21 +1,59 @@
 
 class Action:
-    SOUTH_RIGHT = [0,0,0,1]
-    SOUTH_LEFT = [0,0,0,1]
-    SOUTH_FOWARD = [ ]
-    SOUTH_BACKWORD = [0,0,0,1]
-
-    NORTH_RIGHT = [0,0,0,1]
-    NORTH_LEFT = [0,0,0,1]
-    NORTH_FOWARD = [ ]
-    NORTH_BACKWORD = [0,0,0,1]
-
-    WEST_RIGHT = [0,0,0,1]
-    WEST_LEFT = [0,0,0,1]
-    WEST_FOWARD = [ ]
-    WEST_BACKWORD = [0,0,0,1]
-
-    EAST_RIGHT = [0,0,0,1]
-    EAST_LEFT = [0,0,0,1]
-    EAST_FOWARD = [ ]
-    EAST_BACKWORD = [0,0,0,1]
+    """
+    Each action is a dictionary which contains 4 lists: 
+    North, South, East, West.
+    Each list has 3 values: [Move left, Move right, Go straight]
+    """
+    actions = {
+            # left east and west
+            0: { 'SOUTH' : [0,0,0],
+                'NORTH' : [0,0,0],
+                'EAST' : [1,0,0],
+                'WEST' : [1,0,0]
+            },
+            # left, right and straight west
+            1: { 'SOUTH' : [0,0,0],
+                'NORTH' : [0,0,0],
+                'EAST' : [0,0,0],
+                'WEST' : [1,1,1]
+            },
+            # right and straight east and west
+            2: { 'SOUTH' : [0,0,0],
+                'NORTH' : [0,0,0],
+                'EAST' : [0,1,1],
+                'WEST' : [0,1,1]
+            },
+            # left, right and straight east
+            3: { 'SOUTH' : [0,0,0],
+                'NORTH' : [0,0,0],
+                'EAST' : [1,1,1],
+                'WEST' : [0,0,0] 
+            },
+            # left south and north
+            4: { 'SOUTH' : [1,0,0],
+                'NORTH' : [1,0,0],
+                'EAST' : [0,0,0], 
+                'WEST' : [0,0,0] 
+            },
+            # left, right and straight south
+            5: { 'SOUTH' : [1,1,1],
+                'NORTH' : [0,0,0],
+                'EAST' : [0,0,0], 
+                'WEST' : [0,0,0]
+            },
+            # right and straight south, north
+            5: { 'SOUTH' : [0,1,1],
+                'NORTH' : [0,1,1],
+                'EAST' : [0,0,0],
+                'WEST' : [0,0,0] 
+            }, 
+            # leftright and straight south, north
+            6: { 'SOUTH' : [0,0,0],
+                'NORTH' : [1,1,1],
+                'EAST' : [0,0,0], 
+                'WEST' : [0,0,0] 
+            },
+    }
+    
+    

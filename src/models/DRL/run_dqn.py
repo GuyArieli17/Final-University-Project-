0c6d7d51bb7ac9f415d36927e0b358ed3e8b1b8f
Guy Arieli <guyarieli17@gmail.com>
2021-12-06 14:53:59 +0200

    change hyper params
    change for lower power for more fast run

--- src/models/DRL/run_dqn.py
+++ src/models/DRL/run_dqn.py
@@ -12,17 +12,16 @@ import numpy as np
 from mediator.custom_enviroment import TrafficSteeringEnvironment
 
 
-# MAX_EPISODES = 5_000
-MAX_EPISODES = 10
-EPISODES_BEFORE_TRAIN = 5
+MAX_EPISODES = 2
+EPISODES_BEFORE_TRAIN = 10
 EVAL_EPISODES = 10
 EVAL_INTERVAL = 1
 
 # max steps in each episode, prevent from running too long
 # MAX_STEPS = 10_000  # None
-MAX_STEPS = 1_000  # None
+MAX_STEPS = 500  # None
 
-MEMORY_CAPACITY = 10_000
+MEMORY_CAPACITY = 300
 BATCH_SIZE = 100
 CRITIC_LOSS = "mse"
 MAX_GRAD_NORM = None
@@ -46,6 +45,7 @@ def run(env_class):
     env_eval.seed(RANDOM_SEED)
     env.seed(RANDOM_SEED)
 
+
     state_dim: int = env.observation_space.shape[0]
     if len(env.action_space.shape) >= 1:
         action_dim = env.action_space.shape[0]
@@ -61,33 +61,26 @@ def run(env_class):
               epsilon_decay=EPSILON_DECAY, max_grad_norm=MAX_GRAD_NORM,
               episodes_before_train=EPISODES_BEFORE_TRAIN)
 
-    episodes:list = []
-    eval_rewards:list = []
+    episodes: list = []
+    eval_rewards: list = []
 
     sys.stdout.write('start EPISODES\n')
 
     while dqn.n_episodes < MAX_EPISODES:
-        # sys.stdout.write('episode: ' + str(dqn.n_episodes)+ ', n_steps: ' + str(dqn.n_steps) + '\n')
         dqn.interact()
         if dqn.n_episodes >= EPISODES_BEFORE_TRAIN:
             dqn.train()
-        if dqn.episode_done and ((dqn.n_episodes+1) % EVAL_INTERVAL == 0):
-            # sys.stdout.write('eval ' + str(dqn.n_episodes)+'\n')
-            # rewards, _ = dqn.evaluation(env_eval, EVAL_EPISODES)
-            # rewards_mu, rewards_std = agg_double_list(rewards)
-            # sys.stdout.write("Episode %d, Average Reward %.2f" %
-            #                  (dqn.n_episodes+1, rewards_mu))
-            episodes.append(dqn.n_episodes+1)
-            # eval_rewards.append(rewards_mu)
+        # if dqn.episode_done and ((dqn.n_episodes+1) % EVAL_INTERVAL == 0):
+        #     episodes.append(dqn.n_episodes+1)
 
     sys.stdout.write('done EPISODES\n')
 
-    episodes = np.array(episodes)
-    eval_rewards = np.array(eval_rewards)
-    np.savetxt("./container-output/%s_dqn_episodes.txt" %
-               "TrafficSteeringEnvironment", episodes)
-    np.savetxt("./container-output/%s_dqn_eval_rewards.txt" %
-               "TrafficSteeringEnvironment", eval_rewards)
+    # episodes = np.array(episodes)
+    # eval_rewards = np.array(eval_rewards)
+    # np.savetxt("./container-output/%s_dqn_episodes.txt" %
+    #            "TrafficSteeringEnvironment", episodes)
+    # np.savetxt("./container-output/%s_dqn_eval_rewards.txt" %
+    #            "TrafficSteeringEnvironment", eval_rewards)
 
     sys.stdout.write('done saves\n')
 

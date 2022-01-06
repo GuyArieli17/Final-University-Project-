74734e850ca2a496e466f622ebc4aed5ce7c786a
GuyArieli17 <guyarieli17@gmail.com>
2022-01-06 07:27:51 +0000

    creat feedback loop with eran

--- network/drl/drl_xapp.py
+++ network/drl/drl_xapp.py
@@ -1,84 +1,62 @@
 import json
-from ricxappframe.xapp_frame import RMRXapp, rmr
+from ricxappframe.xapp_frame import Xapp, rmr
 import numpy as np
-from gym import spaces
 from torch import nn
 from torch import optim
-from drl import DQN,ActorNetwork
+from drl import ActorNetwork
+import torch as th
+import time
+
+
 #  ----------------- Const Params -----------------
-_RESET_REQUEST = 60_000
-_STEP_REQUEST = 61_000
-
-_STATE_ACK = 60_001
-_DONE_ACK = 61_001
+START_ACTION = 62_000
+GET_ACTION = 60_000
+ACTION_TYPE = 61_000
 
 
+#  ----------------- Model Params -----------------
+
+ACTION_DIM = 8
+STATE_DIM = 24
+HIDDEN_LAYER = 50
 
 
-#  ----------------- Xapp -----------------
 class DRLXapp:
+    """
+        Todo: add commends
+    """
+    def _loop(self,xapp):
+        print("\t Start loop: ")
+        start_connection = json.dumps({"test_send": 0}).encode()
+        print(f"[SEND] start: {start_connection}")
+        xapp.rmr_send(start_connection, START_ACTION)
+        action = 8
+        while True:
+            action_json = json.dumps({"Action": action}).encode()
+            xapp.rmr_send(action_json, GET_ACTION)
+            for (summary, sbuf) in xapp.rmr_get_messages():
+                message = json.loads(summary[rmr.RMR_MS_PAYLOAD])
+                print("[GOT]: {0}".format(message))
+                next_state, reward, done = message['State']
+                action = int(th.argmax(self._model(th.tensor(next_state).float())))
+                xapp.rmr_free(sbuf)
+            time.sleep(0.01)
 
-    """
-        The Class is abstraction of RMRxapp.
-        Will implment the function we will be needing
-    """
-    def __init__(self, fake_sdl=True) -> None:
+
+    def __init__(self, trained_model_weight_path="./model_params.pth", fake_sdl=True) -> None:
+        """
+            Todo: dsadsada
+        """
+        # Load Model From Memory
+        self._model = ActorNetwork(STATE_DIM, HIDDEN_LAYER, ACTION_DIM, nn.ReLU())
+        self._model.load_state_dict(th.load(trained_model_weight_path))
         self._fake_sdl = fake_sdl
-        self._xapp = RMRXapp(default_handler=self._default_handler,
-                                post_init=self._post_init, use_fake_sdl= self._fake_sdl)
-        ### bind funcation to message type
-        # self._xapp.register_callback(self.send_reset, _RESET_REQUEST)
-        # self._xapp.register_callback(self.send_reset, _STEP_REQUEST)
+        # Create Xapp instace
+        self._xapp = Xapp(entrypoint=self._loop, rmr_port=4564, use_fake_sdl=True)
+        # Register Callback Functions
         
-
-    def send_step(self):
-        #Todo: implement
-        self._xapp.rmr_send(json.dumps({"ping": number}).encode(), 6660666)
-        pass
-
-    def send_reset(self):
-        #Todo: implement
-        self._xapp.rmr_send(json.dumps({"ping": number}).encode(), 6660666)
-
-
-
-    @staticmethod
-    def default_handler(xapp,summary, sbuf):
-        """
-            Todo: add examles
-        """
-        xapp.logger.info("pong default handler called!")
-        print("pong default handler received: {0}".format(summary))
-        xapp.rmr_free(sbuf)
-
-
-
-    @staticmethod
-    def post_init(_xapp):
-        """post init"""
-        print("pong xapp could do some useful stuff here!")
-
-
-
-
-    
-
     def run(self):
         self._xapp.run()
-
-
-class MockEnvironment:
-    
-    def __init__(self, drl_xapp: DRLXapp) -> None:
-        drl_xapp.
-
-    def step(action: int):
-        DRLXapp.
-        pass
-
-    def reset():
-        pass
-
 
 
 if __name__ == "__main__":
